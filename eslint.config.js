dddc8d5bc3489e670fe3611554ccb4b783ec5eb4
Dmitriy Shibaev <dima.shibaev@list.ru>
2025-08-28 22:46:58 +0500

    RT-10 Исправление запуска проверок

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ import prettierConfig from 'eslint-config-prettier';
 
 export default [
   {
-    ignores: ['dist/**', 'node_modules/**', 'public/**', 'vendor/**', 'var/**', 'src/**'],
+    ignores: ['dist/**', 'node_modules/**', 'public/**', 'vendor/**', 'var/**', 'src/**', 'config/**'],
   },
   {
     files: ['assets/**/*.{ts,tsx}'],
